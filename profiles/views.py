175c9f138d60730fb53a10787197e1a5bbfcdd76
Nick Warchol <nwarchol123@icloud.com>
2021-06-14 22:39:53 -0700

    Initial Commit

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .models import Profile
-# Create your views here.
-
+from .forms import ProfileModelForm
 
 def my_profile_view(request):
     profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm(request.POST or None, request.FILES or None, request.FILES or None, instance=profile)
+    confirm = False
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            confirm = True
 
     context = {
         'profile': profile,
+        'form': form,
+        'confirm': confirm,
     }
 
     return render(request, 'profiles/myprofile.html', context)

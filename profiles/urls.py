d9e6c0a6762014d9612f9577cac9f70a6264810b
Nick Warchol <nwarchol123@icloud.com>
2021-07-14 17:31:11 -0700

    initial commit

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,16 +1,28 @@
 from django.urls import path
-from .views import ( my_profile_view,
+from .views import ( 
+    my_profile_view,
     invites_received_view,
-    profiles_list_view, 
+    ProfileDetailView,
+    ProfileListView, 
     invite_profiles_list_view,
     ProfileListView,
+    send_invatation,
+    remove_from_friends,
+    accept_invitation,
+    reject_invitation,
 )
 
 app_name = 'profiles'
 
 urlpatterns = [
-    path('myprofile/', my_profile_view, name='my-profile-view'),
+    path('', ProfileListView.as_view(), name=''),
+    path('myprofile/', my_profile_view, name='my-profile'),
     path('my-invites/', invites_received_view, name='my-invites-view'),
-    path('all-profile/', profiles_list_view, name='all-profile-view'),
+    path('all-profiles/', ProfileListView.as_view(), name='all-profiles-view'),
     path('to-invite/', invite_profiles_list_view, name='invite-profile-view'),
+    path('send-invite/', send_invatation, name='send-invite'),
+    path('remove-friend', remove_from_friends, name='remove-friend'),
+    path('<slug>/', ProfileDetailView.as_view(), name='profile-detail-view'),
+    path('my-invites/accept/', accept_invitation, name='accept-invite'),
+    path('my-invites/reject/', reject_invitation, name='reject-invite'),
 ]
